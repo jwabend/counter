bf2d904f5be997862f14c82510f7a92bd3aa78b1
jwabend <jw.abend@gmail.com>
2022-06-15 18:25:17 -0400

    edit index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+//     obj        module
+//importing the object from the module
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+//import registerServiceWorking from './registerServiceWorker';
+import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./components/counters";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Counters />, document.getElementById('root'));
+
+
